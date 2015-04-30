40fd5fbf8477122d528ed747f83e262bd8e31ca2
MarthSinclaire <dpatt94@yahoo.com>
2015-04-30 01:06:02 -0400

    lab  complete

---
+++ lab 2 README.txt
@@ -0,0 +1,9 @@
+Dalton Patterson
+lab2
+CS 452
+
+For this lab I adapted some keyboard event code from an exercise in another class.
+Functions are in place to operate a set of binary variables.
+The update function first checks to see if the binary variables are true, then if the square is out of bounds, 
+then if the conditions are right then the relevant vertices of the square are updated.
+A call to the update function is included in the render function to ensure it is called every frame.
--- lab2.js
+++ lab2.js
@@ -1,22 +1,119 @@
+// Author: Dalton Patterson
 var gl;
 var points;
+var bufferId;
+var vPosition;
+var program;
+var canvas;
+var keyleft, keyright, keyup, keydown;
 window.onload = function init()
 {
-var canvas = document.getElementById( "gl-canvas" );
+canvas = document.getElementById( "gl-canvas" );
+
+
 gl = WebGLUtils.setupWebGL( canvas );
 if ( !gl ) { alert( "WebGL isn't available" ); }
+
+	points = new Float32Array([.1,.1, 
+				-.1,.1, 
+				.1,-.1, 
+				-.1,-.1]);
+
 //
 // Configure WebGL
 //
 gl.viewport( 0, 0, canvas.width, canvas.height );
-gl.clearColor( 0.0, 0.0, 0.0, 1.0 );
+gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
 // Load shaders and initialize attribute buffers
-var program = initShaders( gl, "vertex-shader", "fragment-shader" );
+program = initShaders( gl, "vertex-shader", "fragment-shader" );
 gl.useProgram( program );
+bufferId = gl.createBuffer();
+gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
+	
+gl.bufferData( gl.ARRAY_BUFFER,points, gl.STATIC_DRAW );
+vPosition = gl.getAttribLocation( program, "vPosition" );
+gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
+gl.enableVertexAttribArray( vPosition );
 // Load the data into the GPU
 // Associate our shader variables with our data buffer
 render();
+update();
 };
-function render() {
-gl.clear( gl.COLOR_BUFFER_BIT );
+
+function update() {
+	if(keyright){
+		if(points[0] < 1){
+			for(var i = 0; i < points.length; i +=2){
+				points[i] += .02;
+			}
+		}
+	}
+	if(keyup){
+		if(points[1] < 1){
+			for(var i = 1; i < points.length; i +=2){
+				points[i] += .02;
+			}
+		}
+	}
+	if(keydown){
+		if(points[5] > -1){
+			for(var i = 1; i < points.length; i +=2){
+				points[i]-= .02;
+			}
+		}
+	}
+	
+	if(keyleft){
+		if(points[2] > -1){
+			for(var i = 0; i < points.length; i +=2){
+				points[i] -= .02;
+			}
+		}
+	}
+//	requestAnimFrame(update);
 }
+
+function render() {
+	update();
+gl.clear( gl.COLOR_BUFFER_BIT );
+gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
+gl.bufferData( gl.ARRAY_BUFFER,points, gl.STATIC_DRAW );
+vPosition = gl.getAttribLocation( program, "vPosition" );
+gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
+gl.enableVertexAttribArray( vPosition );
+gl.drawArrays( gl.TRIANGLE_STRIP, 0, 4 );
+    requestAnimFrame( render );
+}
+				$(document).keydown(function(e) {
+					if(e.which == 68){
+						keyright = true;
+					}
+					
+					else if(e.which == 87){
+						keyup = true;
+					}
+					
+					else if(e.which == 83){
+						keydown = true;
+					}
+					
+					else if(e.which == 65){
+						keyleft = true;
+					}
+					console.log(e.which);
+				});
+
+				$(document).keyup(function(e) {
+					if(e.which == 68){
+						keyright = false;
+					}
+					if(e.which == 87){
+						keyup = false;
+					}
+					if(e.which == 83){
+						keydown = false;
+					}
+					if(e.which == 65){
+						keyleft = false;
+					}
+				});
